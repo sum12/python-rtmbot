25003dce1f3e6d7bf816cfd6c17493a553a346d0
Sumit Jamgade <sumitj@invicto.in>
2015-12-06 19:45:42 +0530

    restart mechanism, to can compensate for ntp updates to system clock

--- plugins/sleeper.py
+++ plugins/sleeper.py
@@ -1,4 +1,5 @@
 import datetime as dt
+import subprocess
 
 import logging
 from lib import *
@@ -12,13 +13,30 @@ process_message = plgn.process_message
 
 totalSleept = 0
 startTime = dt.datetime.now()
-
+count = 0
 
 crontable.append([cron(), 'sleeper'])
 def sleeper():
     global totalSleept
     totalSleept = totalSleept + 1
+    global startTime
+    curr = dt.datetime.now()
+    calc = (dt.timedelta(seconds=totalSleept) + startTime)
+    diff  = (calc - curr)
+    if diff.total_seconds() > 0:
+        global count
+        count += 1
+        if count >= 60:
+            count = 0
+            outputs.append(['random', 'diff is high'])
 
+@command('restart')
+def restart(data, **details):
+    subprocess.Popen(['sudo', 'service', 'rtmbot.init','restart'])
+
+@command('stop')
+def restart(data, **details):
+    subprocess.Popen(['sudo', 'service', 'rtmbot.init','stop'])
 
 @command('slept')
 def time(data, **details):
