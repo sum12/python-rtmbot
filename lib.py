5421eb3c7e5f10f674ca83ce71984087d0528319
Sumit Jamgade <sumitj@invicto.in>
2016-07-14 12:11:04 +0530

    command first letter is only case-optioned if its an alpha

--- lib.py
+++ lib.py
@@ -242,7 +242,9 @@ usage: help   "plgin-name or plgn-number"    "command-name or command-number"
 
     def command(self, regex, help='', private_only=False, restrict_to=None):
         def wrapper(func):
-            rx ='^['+regex[0]+regex[0].upper()+']'+regex[1:] +'$'
+            rx = regex
+            if regex[0].lower() in 'qwertyuiopasdfghjklzxcvbnm':
+                rx ='['+regex[0]+regex[0].upper()+']'+regex[1:] 
             self.funcs.setdefault(rx , (func, help or regex.split()[0], private_only, restrict_to))
             return func
         return wrapper
