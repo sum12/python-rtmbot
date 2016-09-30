96396d950fdf78cef2510993cde74620dfd4ea72
Sumit Jamgade <sumitj@invicto.in>
2016-09-30 11:51:17 +0530

    [repeat] doc string added

--- plugins/repeat/repeat.py
+++ plugins/repeat/repeat.py
@@ -3,6 +3,7 @@ import re
 
 from lib import Plugin
 plgn = Plugin('repeat')
+plgn.__doc__ = "Demo/Dummy plugin"
 
 
 @plgn.command('repeat (?P<what>[-a-zA-Z0-9 `,;!@#$%^&*()_=.{}:"\?\<\>/\[\'\]\\n]+)')
