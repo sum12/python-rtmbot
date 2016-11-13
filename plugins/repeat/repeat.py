0c6a9c67c24fb6776a2f1eae4616976d0f95c623
Sumit Jamgade <sumitj@invicto.in>
2016-11-13 07:48:08 +0530

    testing functionality of maximum parallel executions

--- plugins/repeat/repeat.py
+++ plugins/repeat/repeat.py
@@ -1,7 +1,10 @@
 import time
 import re
 
-from lib import Plugin
+from lib import Plugin, cron
+import logging
+logger = logging.getLogger('bot.repeat')
+logger.setLevel(logging.DEBUG)
 plgn = Plugin('repeat')
 plgn.__doc__ = "Demo/Dummy plugin"
 
@@ -9,4 +12,13 @@ plgn.__doc__ = "Demo/Dummy plugin"
 @plgn.command('repeat (?P<what>[-a-zA-Z0-9 `,;!@#$%^&*()_=.{}:"\?\<\>/\[\'\]\\n]+)')
 def repeat(data, what):
     return 'repeating ' + what
+
+
+
+#@plgn.schedule(cron(second=range(0,60,5)), maximum = 2)
+#def logr(*args, **kwargs):
+#    plgn.myid =myid= getattr(plgn,'myid',0) + 1
+#    logger.info('start myid is %s' % myid)
+#    time.sleep(20)
+#    logger.info('end myid is %s' % myid)
 
