1eb59e3f90b7faf031b980e5de70fd2550f429b0
Sumit Jamgade <sumitj@invicto.in>
2016-11-15 13:29:44 +0530

    [managerclient] adding dumb request to heroku, so dyno does not sleep

--- plugins/managerclient.py
+++ plugins/managerclient.py
@@ -1,4 +1,5 @@
 import requests
+from itertools import repeat
 
 from lib import Plugin, cron
 import logging
@@ -13,14 +14,19 @@ URL = ''
 @plgn.command('exe (?P<config>(\d+,)*\d+)')
 def exercise(data, **details):
     """ Store exe related data """
+    URL = plgn.URL + '/daily' 
     r = requests.post(URL, data={'type':'exercise', 'data':details['config']}, verify=False)
     if r.status_code == 201:
         return 'Ok'
     else:
         return 'Failed to inform the server, please try again'
 
+@plgn.schedule(repeat(25), maximum=1)
+def ping(data, **details):
+    r = requests.get(URL, verify=False)
+
+
 @plgn.setupmethod
 def init(config):
-    global URL
-    URL = config['url']
+    plgn.URL = config['url']
 
